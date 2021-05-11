3011410ccc5b0c3fd04137685282306ff2fa0e6b
Ferenc Dykstra <oneup1@gmail.com>
2021-05-11 14:17:35 +0200

    - Test filling/emptying containers.

--- BucketTests/BucketTest.cs
+++ BucketTests/BucketTest.cs
@@ -1,4 +1,6 @@
 using Buckets.Common.Model;
+using Buckets.ViewModel;
+using Buckets.ViewModel.Event;
 using System;
 using Xunit;
 
@@ -60,6 +62,60 @@ namespace BucketTests
             OilBarrel.Get(158);
             OilBarrel.Get(159);
             Assert.Throws<ArgumentOutOfRangeException>(() => OilBarrel.Get(160));
+        }
+
+        [Fact]
+        public void FillContainers()
+        {
+            ContainerViewModel container = new BucketViewModel(Bucket.GetDefault(4));
+
+            // Test whether content is adjused correctly when filling a container.
+            container.Fill(2, false);
+            Assert.Equal(6, container.Content);
+
+            // Test whether content is adjusted correctly when overflowing a container.
+            container.Fill(100, false); // Should not fill when not forcing.
+            Assert.Equal(6, container.Content);
+            container.Fill(100, true);
+            Assert.Equal(container.Capacity, container.Content);
+
+            // Test whether an exception is thrown when passing invalid ammounts.
+            Assert.Throws<ArgumentOutOfRangeException>(() => container.Fill(0, false));
+            Assert.Throws<ArgumentOutOfRangeException>(() => container.Fill(-2, false));
+
+            // Test whether an event is raised when overflowing a container.
+            bool eventRaised = false;
+            container.Full += delegate(object sender, ContainerFullEventArgs e)
+            {
+                eventRaised = true;
+            };
+            container.Fill(1, false);
+            Assert.True(eventRaised);
+
+            // TODO: Test whether no event is raised when not forcing overflow.
+            // TODO: Test filling buckets with other buckets.
+        }
+
+        [Fact]
+        public void EmptyContainers()
+        {
+            ContainerViewModel container = new OilBarrelViewModel(OilBarrel.Get(100));
+
+            // Test whether content is adjused correctly when emptying a container.
+            container.Empty(25);
+            Assert.Equal(75, container.Content);
+            container.Empty();
+            Assert.Equal(0, container.Content);
+
+            // Test whether an exception is thrown when passing invalid ammounts.
+            Assert.Throws<ArgumentOutOfRangeException>(() => container.Empty(0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => container.Empty(-2));
+            
+            // Test whether an exception is thrown when trying to empty a container
+            // that is already empty or when it contains less than the amount requested.
+            Assert.Throws<InvalidOperationException>(() => container.Empty(100));
+            container.Fill(100, false);
+            Assert.Throws<ArgumentOutOfRangeException>(() => container.Empty(200));
         }
 
         /*[Theory]
